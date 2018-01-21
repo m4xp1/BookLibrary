a9cf054050eca431f71f9cfe4226f92f1ce93c01
Dmitriy Makarov <xcorp.one@gmail.com>
2018-01-21 15:09:03 +0300

    Added methods setTitle and setAppBarScrolled in base activity

--- app/src/main/java/one/xcorp/booklibrary/ui/screen/Activity.java
+++ app/src/main/java/one/xcorp/booklibrary/ui/screen/Activity.java
@@ -1,13 +1,16 @@
 package one.xcorp.booklibrary.ui.screen;
 
 import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 import butterknife.ButterKnife;
 
-@SuppressWarnings("unused")
+@SuppressWarnings({"unused", "SameParameterValue"})
 public abstract class Activity extends AppCompatActivity {
 
     private @Nullable Toolbar toolbar;
@@ -25,7 +28,46 @@ public abstract class Activity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public void setTitle(int resId) {
+        super.setTitle(resId);
+        if (toolbar != null) {
+            toolbar.setTitle(resId);
+        }
+    }
+
+    @Override
+    public void setTitle(CharSequence title) {
+        super.setTitle(title);
+        if (toolbar != null) {
+            toolbar.setTitle(title);
+        }
+    }
+
     public @Nullable Toolbar getToolbar() {
         return toolbar;
+    }
+
+    protected void setAppBarScrolled(boolean scrolled) {
+        AppBarLayout appBar = findViewById(getResources()
+                .getIdentifier("app_bar", "id", getPackageName()));
+
+        if (appBar == null) {
+            return;
+        }
+
+        CoordinatorLayout.LayoutParams params =
+                (CoordinatorLayout.LayoutParams) appBar.getLayoutParams();
+        AppBarLayout.Behavior behavior = (AppBarLayout.Behavior) params.getBehavior();
+
+        if (behavior == null) {
+            params.setBehavior(behavior = new AppBarLayout.Behavior());
+        }
+
+        behavior.setDragCallback(new AppBarLayout.Behavior.DragCallback() {
+            @Override public boolean canDrag(@NonNull AppBarLayout appBarLayout) {
+                return scrolled;
+            }
+        });
     }
 }
