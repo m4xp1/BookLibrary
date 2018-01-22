2ef6310757283953d7dc41b598a0812b034e3aa0
Dmitriy Makarov <xcorp.one@gmail.com>
2018-01-22 06:28:36 +0300

    Added sort list method in utils

--- app/src/main/java/one/xcorp/booklibrary/core/Utils.java
+++ app/src/main/java/one/xcorp/booklibrary/core/Utils.java
@@ -1,5 +1,10 @@
 package one.xcorp.booklibrary.core;
 
+import com.annimon.stream.function.Function;
+
+import java.util.Collections;
+import java.util.List;
+
 public class Utils {
 
     @SuppressWarnings("SameParameterValue")
@@ -13,6 +18,17 @@ public class Utils {
         } catch (NumberFormatException e) {
             return null;
         }
+    }
+
+    public static <T, R extends Comparable<? super R>> List<T> sort(
+            List<T> list, Function<? super T, ? extends R> function) {
+        Collections.sort(list, (t1, t2) -> {
+            final R u1 = function.apply(t1);
+            final R u2 = function.apply(t2);
+            return u1.compareTo(u2);
+        });
+
+        return list;
     }
 
     private Utils() { /* do nothing */ }
